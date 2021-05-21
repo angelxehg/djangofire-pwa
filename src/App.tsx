399a0bffa5a179ede2c7852cc3e95cb8aa2ae6ed
Angel Hurtado <sudo@angelxehg.com>
2021-05-21 11:51:45 -0500

    Mostrar versión 0.1.2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className="container-fluid pt-3 pb-3">
       <header>
-        <h1>Djangofire v0.1.1</h1>
+        <h1>Djangofire v0.1.2</h1>
         <p>
           Hola mundo! Creado por <a target="_blank" rel="noopener noreferrer" href="https://angelxehg.github.io">Angel Hurtado</a>
         </p>
