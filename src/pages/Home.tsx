2561b2fb0bcc3d280c26d5db1ad78e53c719d761
Angel Hurtado <sudo@angelxehg.com>
2021-05-21 11:21:13 -0500

    Inicio de sesión con Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import { useUser } from 'reactfire';
+import { logout } from '../functions/auth';
 
 const HomePage = () => {
   const { data: user } = useUser();
   return (
     <main>
       Ya inició sesión como {user.displayName}
+      <Button onClick={logout} size="sm" variant="danger">
+        Cerrar sesión
+      </Button>
     </main>
   )
 }
